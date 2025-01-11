5ae908e5df108e0a745dde2171b278ea93558ef0
Oğulcan Günaydın <ogulcangunaydin@outlook.com>
2025-01-11 22:42:49 +0300

    Add slider to carousel

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -1,35 +1,26 @@
 // Carousel.js
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import styles from "./Carousel.module.css";
 import { ArrowBackIos, ArrowForwardIos } from "@mui/icons-material";
 import CircularProgress from "@mui/material/CircularProgress";
+import SliderComponent from "@mui/material/Slider";
 
 const Carousel = ({ children }) => {
   const sliderRef = useRef(null);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const totalSlides = React.Children.count(children);
 
   const settings = {
     infinite: true,
-    slidesToShow: 4,
+    slidesToShow: totalSlides < 4 ? totalSlides : totalSlides / 2,
     slidesToScroll: 1,
     speed: 500,
     arrows: false, // Disable default arrows
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-        },
-      },
-    ],
+    dots: false, // Disable dots
+    afterChange: (current) => setCurrentSlide(current),
   };
 
   const previous = () => {
@@ -38,6 +29,11 @@ const Carousel = ({ children }) => {
 
   const next = () => {
     sliderRef.current.slickNext();
+  };
+
+  const handleSliderChange = (event, newValue) => {
+    setCurrentSlide(newValue);
+    sliderRef.current.slickGoTo(newValue);
   };
 
   const isLoading = !children || React.Children.count(children) === 0;
@@ -62,6 +58,43 @@ const Carousel = ({ children }) => {
           </Slider>
           <div className={styles.nextArrow} onClick={next}>
             <ArrowForwardIos fontSize="large" />
+          </div>
+          <div className={styles.scrollSlider}>
+            <SliderComponent
+              value={currentSlide}
+              min={0}
+              max={totalSlides - 1}
+              onChange={handleSliderChange}
+              aria-labelledby="carousel-slider"
+              sx={{
+                color: "#D9D9D9",
+                height: 20, // Height of the track
+                "& .MuiSlider-thumb": {
+                  height: 20,
+                  width: 100,
+                  opacity: 1,
+                  backgroundColor: "#D9D9D9",
+                  borderRadius: 10,
+                  "&:hover, &:focus, &:active": {
+                    boxShadow: "none", // Disable focus effect
+                  },
+                  "& .airbnb-bar": {
+                    height: 9,
+                    width: 1,
+                    backgroundColor: "currentColor",
+                    marginLeft: 1,
+                    marginRight: 1,
+                  },
+                },
+                "& .MuiSlider-track": {
+                  border: "none",
+                },
+                "& .MuiSlider-rail": {
+                  opacity: 0.5,
+                  backgroundColor: "#D9D9D9",
+                },
+              }}
+            />
           </div>
         </>
       )}
